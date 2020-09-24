2fe3632f4e72c412e6c49109ea07bc422d8416cc
jon-kane <51674117+jon-kane@users.noreply.github.com>
2020-09-24 14:59:14 -0500

    Pulls emails from user's email server
    Code pulls emails from user supplied server & displays short form of email to user. User can then select email subject to view entire email. Fixes issues #22 and #23.

--- Email-Client/g9client/forms.py
+++ Email-Client/g9client/forms.py
@@ -1,6 +1,4 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from g9client.models import User
@@ -26,17 +24,12 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
-class UpdateAccoutForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Update')
-
-    def validate_email(self, email):
-        if email.data != current_user.email:
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError('That email is taken. Please choose a different one.')
+# Submit button for syncing mail
+class SyncMailForm(FlaskForm):
+    submit = SubmitField("Sync Mail")
 
 class EmailForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
+    to = StringField('To', validators=[DataRequired()])
+    subject = StringField('Subject', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Send')
