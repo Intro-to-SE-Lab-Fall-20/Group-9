285da617f9859bf27ca4a9e2bcc568cdcf2f3804
jon-kane <51674117+jon-kane@users.noreply.github.com>
2020-11-13 19:32:34 -0600

    Create test-launch-client.py

---
+++ Email-Client/test-launch-client.py
@@ -0,0 +1,26 @@
+import time
+from selenium import webdriver
+from multiprocessing import Process
+
+from g9client import app
+
+server = Process(
+    target=app.run,
+    kwargs = {
+        "host":"127.0.0.1",
+        "port":8080,
+        "debug":False
+    }
+)
+server.start()
+
+# selenium part
+driver = webdriver.Chrome()
+
+driver.get('http://127.0.0.1:8080/');
+time.sleep(1)
+
+driver.quit() # Close the webdriver
+
+server.terminate() # Terminate the flask app
+server.join()
